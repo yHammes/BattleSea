8936d3799fb655cb5e0e604eb7b81920e6e474c1
Hammes <127154896+yHammes@users.noreply.github.com>
2025-11-22 18:50:23 -0300

    Game finished

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,27 @@
 import model.Board;
 
 void main() {
-    IO.println(String.format("Hello and welcome!"));
+    inicializarTabuleiro();
+}
 
+public static void inicializarTabuleiro() {
+    Scanner scanner = new Scanner(System.in);
     Board board = new Board();
-    System.out.println(board);
+    exibirTabuleiro(board);
+
+    while (true) {
+        System.out.println(board.sendMessage());
+
+        String input = scanner.nextLine().trim();
+
+        String response = board.receiveInput(input.toLowerCase());
+        if (board.gameState == -1) break;
+        exibirTabuleiro(board);
+        System.out.println(response);
+    }
+}
+
+public static void exibirTabuleiro(Board board) {
+    System.out.println("===== BATTLE SEA =====\n");
+    System.out.println(board.showBoard());
 }
--- src/model/Cell.java
+++ src/model/Cell.java
@@ -1,29 +1,23 @@
 package model;
 
 public class Cell {
-    private int state;
+    public int state;
 
     public Cell() {
         this.state = 0;
     }
 
-    public int state() {
-        return state;
-    }
-
-    public void placeShip() {
-        state = 2;
-    }
-
-    public void hit() {
-        state = 1;
+    public void setState(int state) {
+        this.state = state;
     }
 
     public String toString() {
         return switch (state) {
+            case 0 -> "~";
             case 1 -> "X";
             case 2 -> "S";
-            default -> "~";
+            case 3 -> "O";
+            default -> "?";
         };
     }
 }
